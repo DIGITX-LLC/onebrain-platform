257a40b27db9770aeeea912c909e1df904e83940
jannatul-fardoush <jannatul@digitxgroup.com>
2025-11-18 09:39:45 +0600

    updated trending section

--- frontend/src/app/component/about/Vo3TrendingSection.tsx
+++ frontend/src/app/component/about/Vo3TrendingSection.tsx
@@ -7,22 +7,22 @@ export default function Vo3TrendingSection() {
   
 
     return (
-        <div className="text-white min-h-screen font-sans px-6">
-            {/* Hailuo Section */}
-            <div className="max-w-6xl mx-auto mb-32">
+        <div className=" max-w-6xl mx-auto  font-sans ">
+            
+           
                 {/* Main Title */}
-                <div className="text-center mb-16">
-                    <h1 className="text-4xl  font-semibold">
+                <div className="text-center  mb-12">
+                    <h1 className="text-5xl  font-semibold">
                         Explore the Power of ChatGPT with OneBr<span className='text-[#69aafa]'>ai</span>n
                     </h1>
-                    <h3 className='text-2xl text-[#D1D5DB] font-semibold mb-2'>ChatGPT in Bangladesh is Now Smarter Than Ever </h3>
+                    <h3 className='text-2xl text-[#D1D5DB] font-semibold mt-2'>ChatGPT in Bangladesh is Now Smarter Than Ever </h3>
                 </div>
 
                 {/* Content Layout */}
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-start mb-16">
+                <div className="grid grid-cols-1 lg:grid-cols-2   mb-16">
                     {/* Left Content */}
-                    <div className="space-y-6 md:mt-20 mt-2">
-                        <p className="text-[#D1D5DB] text-xl font-light ">
+                    <div className="   mt-8">
+                        <p className="text-[#D1D5DB] text-xl font-normal pl-20">
                            Discover the best ChatGPT models—including ChatGPT-4, the newest ChatGPT-5 and more—and see how people in Bangladesh use them for study, work, and creativity. From crafting perfect Bangla blogging content to ChatGPT prompt for photo editing, grammar check, and detailed Bangla tutorial, ChatGPT delivers smart answers in seconds. Learn about ChatGPT pricing, how to use ChatGPT in Bangladesh, and subscribe to a ChatGPT Premium subscription easily with OneBrain to access all features in Bangla or English.
                         </p>
                         
@@ -30,18 +30,17 @@ export default function Vo3TrendingSection() {
                     </div>
 
                     {/* Right card */}
-                    <div className="flex justify-center lg:justify-end">
+                    <div className="flex justify-center  ">
                         <div className="w-full max-w-md">
                             <Image src="/chatGpt section/chatGpt.png" alt="Description" width={500} height={300} />
                         </div>
                     </div>
                 </div>
 
-                {/* Full Width Bottom Text Section */}
-                {/*  */}
+                
             </div>
 
             
-        </div>
+       
     );
 }
